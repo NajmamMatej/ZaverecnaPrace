a2d2e7662912a26513246e03f475ec2987705c5d
Matěj Najman <50733986+NajmamMatej@users.noreply.github.com>
2019-06-18 08:17:08 +0200

    Add files via upload

---
+++ Zaverecna_Pracea/Model/Data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zaverecna_Pracea.Model
+{
+    class Data
+    {
+        private int zustatek;
+
+        public int Zustatek
+        {
+            get { return zustatek; }
+            set { zustatek = value; }
+        }
+        private string historie;
+
+        public string Historie
+        {
+            get { return historie; }
+            set { historie = value; }
+        }
+        private int limit;
+
+
+        public int Limit
+        {
+            get { return limit; }
+            set { limit = value; }
+        }
+
+
+    }
+    
+}
